cae9427a41ba474f77daf1ee6ce2f8ce9c91755e
Edward S. Rice <erice11@unl.edu>
2019-04-17 12:37:26 -0500

    Fixed file extension bug

--- slurm_scripts/make_regions.sh
+++ slurm_scripts/make_regions.sh
@@ -2,8 +2,8 @@
 #SBATCH --error logs/make_regions.err
 #SBATCH --output logs/make_regions.out
 
-slurm_scripts/coverage_to_regions.py $infile > lists/regions.tsv
+slurm_scripts/coverage_to_regions.py $infile > lists/regions.bed
 
 num_lines=$(wc -l < lists/regions.bed)
 ((lines_per_file = (num_lines + num_files - 1) / num_files))
-split -d -a4 -l $lines_per_file lists/regions.tsv regions/r
+split -d -a4 -l $lines_per_file lists/regions.bed regions/r
