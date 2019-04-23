d0bb5d62fad7801bb3e3d3a57805c841955eb478
Edward S. Rice <erice11@unl.edu>
2019-04-23 11:44:54 -0500

    Updated bcftools version

--- slurm_scripts/join.sh
+++ slurm_scripts/join.sh
@@ -2,7 +2,7 @@
 #SBATCH --error logs/join.err
 #SBATCH --output logs/join.out
 
-module load freebayes/1.2 bcftools/1.8
+module load freebayes/1.2 bcftools/1.9
 
 zcat regions/r*.vcf.gz | vcffirstheader | bcftools filter -i 'QUAL>=20' -Ou \
     | bcftools sort -Ov -m $sortmem | vcfuniq | bgzip > variants.Q20.vcf.gz
