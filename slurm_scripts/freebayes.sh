6c508b22584a8a4b4eea926307258b16ffe3df47
Edward Rice <edward.rice@missouri.edu>
2020-03-04 13:16:36 -0800

    Changed to freebayes v1.3

--- slurm_scripts/freebayes.sh
+++ slurm_scripts/freebayes.sh
@@ -2,7 +2,7 @@
 #SBATCH --error logs/freebayes.%a.err
 #SBATCH --output logs/freebayes.%a.out
 
-module load freebayes/1.2
+module load freebayes/1.3
 
 ((chunk_number = SLURM_ARRAY_TASK_ID - 1))
 chunk=$(printf "r%04d" $chunk_number)
